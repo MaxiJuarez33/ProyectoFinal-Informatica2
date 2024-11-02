53646328fb5d870f1c7aab7cf5f48a067034437d
juarezmaxi8@gmail.com <juarezmaxi8@gmail.com>
2024-11-02 18:06:05 -0300

    Revert "Lista de dispositivos final"
    This reverts commit 65b07a0c550afa0cdb3395002d7130faf1c5afcf.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,16 @@
 
 int main()
 {
+  std::string maxVerstappen;
+  double checoPerez;
+  Device device;
+  // device.saveDevice("sensor de fantasmas", "testo", 2.2, 1.1, 33);
+  // device.saveDevice("sensor de otaku", "agua", 222.22, 112.1, 2);
+  // device.saveDevice("testo", "tes", 1, 0, 22);
+  maxVerstappen = device.getDeviceByID(3).getName();
+  checoPerez = device.getDeviceByKeyword("fantasmas").getMaxValue();
+  std::cout << maxVerstappen << std::endl;
+  std::cout << checoPerez << std::endl;
   Device device;
 
   std::string nambre;
